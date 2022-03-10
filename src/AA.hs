2e20be53a420274969bd266b0a8d21d6e4b9b192
Pedro Samuel Fagundez <samuelfagundez97@gmail.com>
2022-03-09 21:50:05 -0400

    Lookup ready but without Maybe

--- src/AA.hs
+++ src/AA.hs
@@ -6,6 +6,8 @@ aa (x,a) = x
 -- k es el tipo de claves de busqueda en el diccionario string, number, whatever
 -- a es el tipo de los valores almacenados en el diccionario
 
+-- video 3 2:01 functor
+
 data AA k a = Empty
             -- | Invalido porque no respeta la secuencia de niveles
             | Node {
@@ -19,12 +21,16 @@ data AA k a = Empty
 arbol = Node 0 1 "Uno" (Node 1 2 "Dos" Empty Empty) (Node 1 3 "Tres" Empty Empty)
 arbolVacio = Empty
 
-foldAA :: (a -> b -> b) -> b -> AA k a -> b
-foldAA f b Empty = b
-foldAA f b (Node lv k v l r) = foldAA f (f v (foldAA f b r)) l
+foldAAByValue :: (a -> b -> b) -> b -> AA k a -> b
+foldAAByValue f b Empty = b
+foldAAByValue f b (Node lv k v l r) = foldAAByValue f (f v (foldAAByValue f b r)) l
+
+foldAAById :: (k -> b -> b) -> b -> AA k a -> b
+foldAAById f b Empty = b
+foldAAById f b (Node lv k v l r) = foldAAById f (f k (foldAAById f b r)) l
 
 instance Foldable (AA a) where 
-    foldr = foldAA
+    foldr = foldAAByValue
 
 empty :: AA k a
 empty = Empty
@@ -38,13 +44,16 @@ insert :: k -> v -> AA k v -> AA k v
 -- nodo, creo que hace falta otra funcion para eso
 insert k v a = if isEmpty a then Node 0 k v Empty Empty else Empty
 
-lookup' :: Eq k => k -> AA k v -> Bool
+
+-- TODO: Falta poner un maybe para devolver SiEsta | NoEsta en vez de True | False
+lookup' :: Eq k => k -> AA k a -> Bool
 lookup' k Empty = False
-lookup' k (Node lvl key val Empty Empty) = key == k
-lookup' k (Node lvl key val lAA Empty) = if key == k then key == k else lookup' k lAA
-lookup' k (Node lvl key val Empty rAA) = if key == k then key == k else lookup' k rAA
--- Esto esta malo porque solo va a buscar en el arbol izquierdo
-lookup' k (Node lvl key val lAA rAA) = if key == k then key == k else lookup' k lAA
+lookup' k tree = foldAAById (\key acc -> acc || key == k) False tree
+
+-- Esta es la estructura para buscar y devolver el maybe
+-- foldAAById :: (k -> b -> b) -> b -> AA k a -> b
+-- foldAAById f b Empty = b
+-- foldAAById f b (Node lv k v l r) = foldAAById f (f k (foldAAById f b r)) l
 
 checkInvariants :: AA k a -> AA k a
 checkInvariants a = Empty
